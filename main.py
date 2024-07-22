a1a0466238ecff94b9213819f2a6e927d72e322c
Jessica-1411 <110350487+Jessica-1411@users.noreply.github.com>
2024-07-22 16:30:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,325 @@
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+# Import Selenium automation code
+import pandas as pd
+import openai
+import streamlit as st
+import matplotlib.pyplot as plt
+import os
+import io
+# from scraping import automate
+#import streamlit_nested_layout
+# from classes import get_primer,format_question,run_request
+from langchain import HuggingFaceHub, LLMChain,PromptTemplate
+import warnings
+# from selenium.webdriver.common.keys import Keys
+# import scraping
+warnings.filterwarnings("ignore")
+st.set_option('deprecation.showPyplotGlobalUse', False)
+# st.set_page_config(page_icon="chat2vis.png",layout="wide",page_title="Datavizify")
+
+
+st.markdown("<h1 style='text-align: center; font-weight:bold; font-family: OCR A Extended; padding-top: 0rem;'> \
+            DataVizify</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center;padding-top: 0rem;'>Creating Visualisations using Natural Language \
+            with ChatGPT</h2>", unsafe_allow_html=True)
+
+
+
+def run_request(question_to_ask, model_type, key, alt_key):
+    if model_type == "gpt-4" or model_type == "gpt-3.5-turbo" :
+        # Run OpenAI ChatCompletion API
+        task = "Generate Python Code Script."
+        if model_type == "gpt-4":
+            # Ensure GPT-4 does not include additional comments
+            task = task + " The script should only include code, no comments."
+        openai.api_key = key
+        response = openai.ChatCompletion.create(model=model_type,
+            messages=[{"role":"system","content":task},{"role":"user","content":question_to_ask}])
+        llm_response = response["choices"][0]["message"]["content"]
+    elif model_type == "text-davinci-003" or model_type == "gpt-3.5-turbo-instruct":
+        # Run OpenAI Completion API
+        openai.api_key = key
+        response = openai.Completion.create(engine=model_type,prompt=question_to_ask,temperature=0,max_tokens=500,
+                    top_p=1.0,frequency_penalty=0.0,presence_penalty=0.0,stop=["plt.show()"])
+        llm_response = response["choices"][0]["text"] 
+    else:
+        # Hugging Face model
+        llm = HuggingFaceHub(huggingfacehub_api_token = alt_key, repo_id="codellama/" + model_type, model_kwargs={"temperature":0.1, "max_new_tokens":500})
+        llm_prompt = PromptTemplate.from_template(question_to_ask)
+        llm_chain = LLMChain(llm=llm,prompt=llm_prompt)
+        llm_response = llm_chain.predict()
+    # rejig the response
+    llm_response = format_response(llm_response)
+    return llm_response
+
+def format_response( res):
+    # Remove the load_csv from the answer if it exists
+    csv_line = res.find("read_csv")
+    if csv_line > 0:
+        return_before_csv_line = res[0:csv_line].rfind("\n")
+        if return_before_csv_line == -1:
+            # The read_csv line is the first line so there is nothing to need before it
+            res_before = ""
+        else:
+            res_before = res[0:return_before_csv_line]
+        res_after = res[csv_line:]
+        return_after_csv_line = res_after.find("\n")
+        if return_after_csv_line == -1:
+            # The read_csv is the last line
+            res_after = ""
+        else:
+            res_after = res_after[return_after_csv_line:]
+        res = res_before + res_after
+    return res
+
+def format_question(primer_desc,primer_code , question, model_type):
+    # Fill in the model_specific_instructions variable
+    instructions = ""
+    if model_type == "Code Llama":
+        # Code llama tends to misuse the "c" argument when creating scatter plots
+        instructions = "\nDo not use the 'c' argument in the plot function, use 'color' instead and only pass color names like 'green', 'red', 'blue'."
+    primer_desc = primer_desc.format(instructions)  
+    # Put the question at the end of the description primer within quotes, then add on the code primer.
+    return  '"""\n' + primer_desc + question + '\n"""\n' + primer_code
+
+def get_primer(df_dataset,df_name):
+    # Primer function to take a dataframe and its name
+    # and the name of the columns
+    # and any columns with less than 20 unique values it adds the values to the primer
+    # and horizontal grid lines and labeling
+    primer_desc = "Use a dataframe called df from data_file.csv with columns '" \
+        + "','".join(str(x) for x in df_dataset.columns) + "'. "
+    for i in df_dataset.columns:
+        if len(df_dataset[i].drop_duplicates()) < 20 and df_dataset.dtypes[i]=="O":
+            primer_desc = primer_desc + "\nThe column '" + i + "' has categorical values '" + \
+                "','".join(str(x) for x in df_dataset[i].drop_duplicates()) + "'. "
+        elif df_dataset.dtypes[i]=="int64" or df_dataset.dtypes[i]=="float64":
+            primer_desc = primer_desc + "\nThe column '" + i + "' is type " + str(df_dataset.dtypes[i]) + " and contains numeric values. "   
+    primer_desc = primer_desc + "\nLabel the x and y axes appropriately."
+    primer_desc = primer_desc + "\nAdd a title. "
+    primer_desc = primer_desc + "{}" # Space for additional instructions if needed
+    primer_desc = primer_desc + "\nUsing Python version 3.9.12, create a script using the dataframe df to graph the following: "
+    pimer_code = "import pandas as pd\nimport matplotlib.pyplot as plt\n"
+    pimer_code = pimer_code + "fig,ax = plt.subplots(1,1,figsize=(5,4))\n"
+    pimer_code = pimer_code + "ax.spines['top'].set_visible(False)\nax.spines['right'].set_visible(False) \n"
+    pimer_code = pimer_code + "df=" + df_name + ".copy()\n"
+    return primer_desc,pimer_code
+
+
+
+def generate_plot():
+    img_data = io.BytesIO()
+    
+    plt.savefig(img_data, format='png')
+    timestamp = int(time.time())
+    filename = f"plot_{timestamp}.png"
+    path=f'C:/Users/91884/Documents/projects/streamlit_projects/datavizify/{filename}'
+    with open(filename, "wb") as f:
+        f.write(img_data.getvalue())
+    plot_area = st.empty()
+    plot_area.image(img_data)
+    
+    options = Options()
+    options.add_argument("--headless")  # Run in headless mode
+# Modify the path below to the location of your Chrome executable
+    options.binary_location = "C:/Program Files/Google/Chrome/Application/chrome.exe"
+
+# Initialize Chrome WebDriver with options
+    driver = webdriver.Chrome(options=options)
+
+# Navigate to the website
+    driver.get("https://imagedescriptiongenerator.xyz/en")
+
+    try:
+    # Find the input element for uploading an image
+        upload_input = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'input[type="file"]'))
+            )
+
+    # Upload the image
+        upload_input.send_keys(path)
+
+    # Find the button element by class name and click it
+        generate_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'button.bg-blue-600'))
+            )
+        generate_button.click()
+
+    # Wait for the description to be generated
+        time.sleep(20)  # Wait for 10 seconds, adjust as needed
+
+    # Find the description element
+        description_element = WebDriverWait(driver, 60).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'div.bg-gray-50'))
+            )
+
+    # Retrieve the text content of the description textbox
+        description_text = description_element.text.strip()
+
+    # Print the generated description
+        print("Generated Description:", description_text)
+        st.write(description_text)
+    except Exception as e:
+            print("An error occurred while getting the description:", e)
+
+    finally:
+    # Close the browser window
+            driver.quit()
+        
+
+available_models = {"ChatGPT-3.5":"gpt-3.5-turbo"}
+
+# List to hold datasets
+if "datasets" not in st.session_state:
+    datasets = {}
+    # Preload datasets
+    datasets["Movies"] = pd.read_csv("movies.csv")
+    datasets["Housing"] =pd.read_csv("housing.csv")
+    datasets["Cars"] =pd.read_csv("cars.csv")
+    datasets["Colleges"] =pd.read_csv("colleges.csv")
+    datasets["Customers & Products"] =pd.read_csv("customers_and_products_contacts.csv")
+    datasets["Department Store"] =pd.read_csv("department_store.csv")
+    datasets["Energy Production"] =pd.read_csv("energy_production.csv")
+    st.session_state["datasets"] = datasets
+else:
+    # use the list already loaded
+    datasets = st.session_state["datasets"]
+
+key_col1,key_col2 = st.columns(2)
+openai_key = ""
+hf_key = ""
+
+with st.sidebar:
+    # First we want to choose the dataset, but we will fill it with choices once we've loaded one
+    dataset_container = st.empty()
+
+    # Add facility to upload a dataset
+    try:
+        uploaded_file = st.file_uploader(":computer: Load a CSV file:", type="csv")
+        index_no=0
+        if uploaded_file:
+            # Read in the data, add it to the list of available datasets. Give it a nice name.
+            file_name = uploaded_file.name[:-4].capitalize()
+            datasets[file_name] = pd.read_csv(uploaded_file)
+            # We want to default the radio button to the newly added dataset
+            index_no = len(datasets)-1
+    except Exception as e:
+        st.error("File failed to load. Please select a valid CSV file.")
+        print("File failed to load.\n" + str(e))
+    # Radio buttons for dataset choice
+    chosen_dataset = dataset_container.radio(":bar_chart: Choose your data:",datasets.keys(),index=index_no)#,horizontal=True,)
+
+    # # Check boxes for model choice
+    # st.write(":brain: Choose your model(s):")
+    # # Keep a dictionary of whether models are selected or not
+    # use_model = {}
+    # for model_desc,model_name in available_models.items():
+    #     label = f"{model_desc} ({model_name})"
+    #     key = f"key_{model_desc}"
+    #     use_model[model_desc] = st.checkbox(label,value=True,key=key)
+ 
+ # Text area for query
+question = st.text_area(":eyes: What would you like to visualise?",height=10)
+go_btn = st.button("Go...")
+
+# Make a list of the models which have been selected
+selected_models = [model_name for model_name, choose_model in available_models.items() if choose_model]
+model_count = len(selected_models)
+
+# Execute chatbot query
+if go_btn and model_count > 0:
+    api_keys_entered = True
+    # Check API keys are entered.
+    if  "ChatGPT-4" in selected_models or "ChatGPT-3.5" in selected_models or "GPT-3" in selected_models or "GPT-3.5 Instruct" in selected_models:
+        if not openai_key.startswith('sk-'):
+            st.error("Please enter a valid OpenAI API key.")
+            api_keys_entered = False
+    if "Code Llama" in selected_models:
+        if not hf_key.startswith('hf_'):
+            st.error("Please enter a valid HuggingFace API key.")
+            api_keys_entered = False
+    if api_keys_entered:
+        # Place for plots depending on how many models
+        plots = st.columns(model_count)
+        # Get the primer for this dataset
+        primer1,primer2 = get_primer(datasets[chosen_dataset],'datasets["'+ chosen_dataset + '"]') 
+        # Create model, run the request and print the results
+        for plot_num, model_type in enumerate(selected_models):
+            with plots[plot_num]:
+                # st.subheader(model_type)
+                try:
+                    # Format the question 
+                    question_to_ask = format_question(primer1, primer2, question, model_type)   
+            # Run the question
+                    answer=""
+                    answer = run_request(question_to_ask, available_models[model_type], key=openai_key,alt_key=hf_key)
+            # the answer is the completed Python script so add to the beginning of the script to it.
+                    answer = primer2 + answer
+            # Generate the plot
+                    # print(answer)
+                    exec(answer)
+                    
+            # Save the plot to a file-like object
+            #         img_data = io.BytesIO()
+            #         plt.savefig(img_data, format='png')
+            # # Save the file to your system
+            #         filename = f"plot_{model_type}.png"
+            #         with open(filename, "wb") as f:
+            #             f.write(img_data.getvalue())
+            # # Send the file path to the file upload input element using Selenium
+            #         # upload_input = driver.find_element(By.CSS_SELECTOR, 'input[type="file"]')
+            #         # upload_input.send_keys(os.path.abspath(filename))
+            # # Display the plot
+            #         plot_area = st.empty()
+            #         plot_area.image(img_data)  
+                    generate_plot()        
+                except Exception as e:
+                    if type(e) == openai.error.APIError:
+                        st.error("OpenAI API Error. Please try again a short time later. (" + str(e) + ")")
+                    elif type(e) == openai.error.Timeout:
+                        st.error("OpenAI API Error. Your request timed out. Please try again a short time later. (" + str(e) + ")")
+                    elif type(e) == openai.error.RateLimitError:
+                        st.error("OpenAI API Error. You have exceeded your assigned rate limit. (" + str(e) + ")")
+                    elif type(e) == openai.error.APIConnectionError:
+                        st.error("OpenAI API Error. Error connecting to services. Please check your network/proxy/firewall settings. (" + str(e) + ")")
+                    elif type(e) == openai.error.InvalidRequestError:
+                        st.error("OpenAI API Error. Your request was malformed or missing required parameters. (" + str(e) + ")")
+                    elif type(e) == openai.error.AuthenticationError:
+                        st.error("Please enter a valid OpenAI API Key. (" + str(e) + ")")
+                    elif type(e) == openai.error.ServiceUnavailableError:
+                        st.error("OpenAI Service is currently unavailable. Please try again a short time later. (" + str(e) + ")")               
+                    else:
+                        st.error("Unfortunately the code generated from the model contained errors and was unable to execute.")
+
+# Display the datasets in a list of tabs
+# Create the tabs
+tab_list = st.tabs(datasets.keys())
+
+# Load up each tab with a dataset
+for dataset_num, tab in enumerate(tab_list):
+    with tab:
+        # Can't get the name of the tab! Can't index key list. So convert to list and index
+        dataset_name = list(datasets.keys())[dataset_num]
+        st.subheader(dataset_name)
+        st.dataframe(datasets[dataset_name],hide_index=True)
+
+# Insert footer to reference dataset origin  
+footer="""<style>.footer {position: fixed;left: 0;bottom: 0;width: 100%;text-align: center;}</style><div class="footer">
+<p> <a style='display: block; text-align: center;'> Datasets courtesy of NL4DV, nvBench and ADVISor </a></p></div>"""
+# st.caption("Datasets courtesy of NL4DV, nvBench and ADVISor")cd doc
+
+# Hide menu and footer
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
